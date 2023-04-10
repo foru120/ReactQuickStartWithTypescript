f04173d3fd2f2c0c2cab26c4752a62565557ae62
foru120 <broodsky1122@hanmail.net>
2023-04-11 00:09:14 +0900

    라우팅을 적용한 예제 실습

---
+++ ch10/todolist-app-router/src/App.tsx
@@ -0,0 +1,35 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from './components/Layout';
+
+import Home from './pages/Home';
+import About from './pages/About';
+import TodoList from './pages/TodoList';
+import AddTodo from './pages/AddTodo';
+import EditTodo from './pages/EditTodo';
+import NotFound from './pages/NotFound';
+
+import { CallbacksType, StatesType } from "./AppContainer";
+
+export type Props = {
+  callbacks: CallbacksType;
+  states: StatesType;
+};
+
+const App = ({states, callbacks}: Props) => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="todos" element={<TodoList states={states} callbacks={callbacks} />} />
+          <Route path="todos/add" element={<AddTodo callbacks={callbacks} />} />
+          <Route path="todos/edit/:id" element={<EditTodo states={states} callbacks={callbacks} />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
